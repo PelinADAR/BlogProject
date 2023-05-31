6479a30b48e4a04cec75bda69702eaa8f7342b4d
PelinADAR <114329608+PelinADAR@users.noreply.github.com>
2023-05-31 14:15:32 +0300

    BlogProject
    This application, which I developed using the Onion Architecture architectural structure, allows different types of users to log in to the system and perform post, author addition, deletion and update operations.

---
+++ BlogProject/HS8_BLogProject.WebAPI/Controllers/GenreController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HS8_BLogProject.WebAPI.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly IGenreService _genreService;
+
+     
+
+        [HttpGet]
+        public IActionResult GetAllGenres()
+        {
+            
+            return View();
+        }
+    }
+}
